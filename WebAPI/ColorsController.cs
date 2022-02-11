8ac61bbdb9f99f1065423fa13ba9c0c2eaaed4ed
Bedirhan Gurler <bgurler001@gmail.com>
2022-02-11 17:33:58 +0300

    Colors API Done

---
+++ WebAPI/ColorsController.cs
@@ -0,0 +1,76 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ColorsController: ControllerBase
+    {
+        IColorService _colorService;
+        public ColorsController(IColorService colorService)
+        {
+            _colorService = colorService;
+        }
+
+        [HttpGet("getallcolors")]
+        public IActionResult GetAllColors()
+        {
+            var result = _colorService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getcolorsbyid")]
+        public IActionResult GetColorsById(int id)
+        {
+            var result = _colorService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("addcolor")]
+        public IActionResult AddColor(Color color)
+        {
+            var result = _colorService.Add(color);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPut("updatecolor")]
+        public IActionResult UpdateColor(Color color)
+        {
+            var result = _colorService.Update(color);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpDelete("deletecolor")]
+        public IActionResult DeleteColor(int id)
+        {
+            var result = _colorService.Delete(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
